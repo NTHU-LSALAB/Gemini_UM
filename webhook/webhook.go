6acd95b9dc79b68ede0c289b0d54c73eaee95f64
cjobr <s210140@shsh.tw>
2020-11-16 16:46:21 +0000

    add webhook

---
+++ webhook/webhook.go
@@ -0,0 +1,439 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/golang/glog"
+	"k8s.io/api/admission/v1beta1"
+	admissionregistrationv1beta1 "k8s.io/api/admissionregistration/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/kubernetes/pkg/apis/core/v1"
+)
+
+var (
+	runtimeScheme = runtime.NewScheme()
+	codecs        = serializer.NewCodecFactory(runtimeScheme)
+	deserializer  = codecs.UniversalDeserializer()
+
+	// (https://github.com/kubernetes/kubernetes/issues/57982)
+	defaulter = runtime.ObjectDefaulter(runtimeScheme)
+)
+
+var (
+	ignoredNamespaces = []string{
+		metav1.NamespaceSystem,
+		metav1.NamespacePublic,
+	}
+	requiredLabels = []string{
+		nameLabel,
+		gpuLabel,
+
+	}
+	addLabels = map[string]string{
+		nameLabel:      NA,
+
+	}
+
+)
+
+const (
+	admissionWebhookAnnotationValidateKey = "admission-webhook-example.qikqiak.com/validate"
+	admissionWebhookAnnotationMutateKey   = "admission-webhook-example.qikqiak.com/mutate"
+	admissionWebhookAnnotationStatusKey   = "admission-webhook-example.qikqiak.com/status"
+
+	nameLabel      = "app.kubernetes.io/name"
+  gpuLabel       = "use-gpu"
+
+	NA = "not_available"
+)
+
+type WebhookServer struct {
+	server *http.Server
+}
+
+// Webhook Server parameters
+type WhSvrParameters struct {
+	port           int    // webhook server port
+	certFile       string // path to the x509 certificate for https
+	keyFile        string // path to the x509 private key matching `CertFile`
+	sidecarCfgFile string // path to sidecar injector configuration file
+}
+
+type patchOperation struct {
+	Op    string      `json:"op"`
+	Path  string      `json:"path"`
+	Value interface{} `json:"value,omitempty"`
+}
+
+func init() {
+	_ = corev1.AddToScheme(runtimeScheme)
+	_ = admissionregistrationv1beta1.AddToScheme(runtimeScheme)
+	// defaulting with webhooks:
+	// https://github.com/kubernetes/kubernetes/issues/57982
+	_ = v1.AddToScheme(runtimeScheme)
+}
+
+func admissionRequired(ignoredList []string, admissionAnnotationKey string, metadata *metav1.ObjectMeta) bool {
+	// skip special kubernetes system namespaces
+	if metadata == nil {
+			glog.Infof("Skip validation ~~~~ buz not a pod")
+			return false
+	}
+	for _, namespace := range ignoredList {
+		if metadata.Namespace == namespace {
+			glog.Infof("Skip validation for %v for it's in special namespace:%v", metadata.Name, metadata.Namespace)
+			return false
+		}
+	}
+
+	annotations := metadata.GetAnnotations()
+
+	if annotations == nil {
+		annotations = map[string]string{}
+	}
+
+	var required bool
+	switch strings.ToLower(annotations[admissionAnnotationKey]) {
+	default:
+		required = true
+	case "n", "no", "false", "off":
+		required = false
+	}
+	return required
+}
+
+func mutationRequired(ignoredList []string, metadata *metav1.ObjectMeta) bool {
+	required := admissionRequired(ignoredList, admissionWebhookAnnotationMutateKey, metadata)
+	annotations := metadata.GetAnnotations()
+	if annotations == nil {
+		annotations = map[string]string{}
+	}
+	status := annotations[admissionWebhookAnnotationStatusKey]
+
+	if strings.ToLower(status) == "mutated" {
+		required = false
+	}
+
+	glog.Infof("Mutation policy for %v/%v: required:%v", metadata.Namespace, metadata.Name, required)
+
+	return required
+}
+
+func validationRequired(ignoredList []string, metadata *metav1.ObjectMeta) bool {
+	required := admissionRequired(ignoredList, admissionWebhookAnnotationValidateKey, metadata)
+	glog.Infof("Validation policy for %v/%v: required:%v", metadata.Namespace, metadata.Name, required)
+	return required
+}
+
+func updateAnnotation(target map[string]string, added map[string]string) (patch []patchOperation) {
+	for key, value := range added {
+		if target == nil || target[key] == "" {
+			target = map[string]string{}
+			patch = append(patch, patchOperation{
+				Op:   "add",
+				Path: "/metadata/annotations",
+				Value: map[string]string{
+					key: value,
+				},
+			})
+		} else {
+			patch = append(patch, patchOperation{
+				Op:    "replace",
+				Path:  "/metadata/annotations/" + key,
+				Value: value,
+			})
+		}
+	}
+	return patch
+}
+
+func updateLabels(target map[string]string, added map[string]string) (patch []patchOperation) {
+	values := make(map[string]string)
+	for key, value := range added {
+		if target == nil || target[key] == "" {
+			values[key] = value
+		}
+	}
+	patch = append(patch, patchOperation{
+		Op:    "add",
+		Path:  "/metadata/labels",
+		Value: values,
+	})
+	return patch
+}
+func addVolume(pod *corev1.Pod) (patch []patchOperation) {
+
+	volume := corev1.Volume{
+		Name: "sharedata",
+		VolumeSource: corev1.VolumeSource{
+			HostPath: &corev1.HostPathVolumeSource{Path:"/home/cjobr" },
+	  },
+
+  }
+	volume2 := corev1.Volume{
+		Name: "ipc",
+		VolumeSource: corev1.VolumeSource{
+			HostPath: &corev1.HostPathVolumeSource{Path:"/tmp" },
+	  },
+	}
+
+
+	path := "/spec/volumes"
+	var value interface{}
+  var value2 interface{}
+
+	path = path + "/-"
+	value = volume
+  value2 = volume2
+	patch = append(patch, patchOperation{
+		Op:    "add",
+		Path:  path,
+		Value: value,
+	})
+	patch = append(patch, patchOperation{
+		Op:    "add",
+		Path:  path,
+		Value: value2,
+	})
+	return patch
+}
+func appendVolumeMountIfMissing(slice []corev1.VolumeMount, v corev1.VolumeMount) []corev1.VolumeMount {
+	for _, ele := range slice {
+		if ele == v {
+			return slice
+		}
+	}
+	return append(slice, v)
+}
+
+func addVolumeMount(containers []corev1.Container) []corev1.Container {
+
+	modifiedContainers := []corev1.Container{}
+
+	for _, container := range containers {
+		  if !container.Resources.Limits["nvidia.com/gpu"] {
+				volumeMount := corev1.VolumeMount{
+					Name:      "sharedata",
+					MountPath: "/home/cjobr",
+				}
+				volumeMount2 := corev1.VolumeMount{
+					Name:      "ipc",
+					MountPath: "/tmp",
+				}
+				//we don't want to mount the same path twice
+				container.VolumeMounts = appendVolumeMountIfMissing(container.VolumeMounts, volumeMount)
+				container.VolumeMounts = appendVolumeMountIfMissing(container.VolumeMounts, volumeMount2)
+			}
+
+		modifiedContainers = append(modifiedContainers, container)
+	}
+
+	return modifiedContainers
+}
+func addContainer(added []corev1.Container) (patch []patchOperation) {
+
+		patch = append(patch, patchOperation {
+				Op:    "replace",
+				Path:  "/spec/containers",
+				Value: added,
+		})
+    return patch
+}
+func createPatch(pod *corev1.Pod,availableAnnotations map[string]string, annotations map[string]string, availableLabels map[string]string, labels map[string]string) ([]byte, error) {
+	var patch []patchOperation
+
+	patch = append(patch, updateAnnotation(availableAnnotations, annotations)...)
+	patch = append(patch, updateLabels(availableLabels, labels)...)
+  patch = append(patch,addVolume(pod)...)
+
+	patch = append(patch,addContainer(addVolumeMount(pod.Spec.Containers))...)
+
+
+	return json.Marshal(patch)
+}
+
+// validate deployments and services
+func (whsvr *WebhookServer) validate(ar *v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
+	req := ar.Request
+	var (
+		availableLabels                 map[string]string
+		objectMeta                      *metav1.ObjectMeta
+		resourceNamespace, resourceName string
+	)
+
+	glog.Infof("AdmissionReview for Kind=%v, Namespace=%v Name=%v (%v) UID=%v patchOperation=%v UserInfo=%v",
+		req.Kind, req.Namespace, req.Name, resourceName, req.UID, req.Operation, req.UserInfo)
+
+	switch req.Kind.Kind {
+	case "Pod":
+		var pod corev1.Pod
+		if err := json.Unmarshal(req.Object.Raw, &pod); err != nil {
+			glog.Errorf("Could not unmarshal raw object: %v", err)
+			return &v1beta1.AdmissionResponse{
+				Result: &metav1.Status{
+					Message: err.Error(),
+				},
+			}
+		}
+		resourceName, resourceNamespace, objectMeta = pod.Name, pod.Namespace, &pod.ObjectMeta
+		availableLabels = pod.Labels
+
+	}
+
+	if !validationRequired(ignoredNamespaces, objectMeta) {
+		glog.Infof("Skipping validation for %s/%s due to policy check", resourceNamespace, resourceName)
+		return &v1beta1.AdmissionResponse{
+			Allowed: true,
+		}
+	}
+
+	allowed := true
+	var result *metav1.Status
+	glog.Info("available labels:", availableLabels)
+	glog.Info("required labels", requiredLabels)
+	for _, rl := range requiredLabels {
+		if _, ok := availableLabels[rl]; !ok {
+			allowed = false
+			result = &metav1.Status{
+				Reason: "required labels are not set",
+			}
+			break
+		}
+	}
+
+	return &v1beta1.AdmissionResponse{
+		Allowed: allowed,
+		Result:  result,
+	}
+}
+
+// main mutation process
+func (whsvr *WebhookServer) mutate(ar *v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
+	req := ar.Request
+	var (
+		availableLabels, availableAnnotations map[string]string
+		objectMeta                            *metav1.ObjectMeta
+		resourceNamespace, resourceName       string
+		pod                                   corev1.Pod
+	)
+
+	glog.Infof("AdmissionReview for Kind=%v, Namespace=%v Name=%v (%v) UID=%v patchOperation=%v UserInfo=%v",
+		req.Kind, req.Namespace, req.Name, resourceName, req.UID, req.Operation, req.UserInfo)
+
+
+	if req.Kind.Kind == "Pod" {
+			if err := json.Unmarshal(req.Object.Raw, &pod); err != nil {
+					glog.Infof("Could not unmarshal raw object: %v", err)
+					return &v1beta1.AdmissionResponse{
+							Result: &metav1.Status{
+									Message: err.Error(),
+							},
+					}
+			}
+			resourceNamespace, resourceName, objectMeta = pod.Namespace, pod.Name, &pod.ObjectMeta
+			availableLabels = pod.Labels
+	}
+
+
+
+
+	if !mutationRequired(ignoredNamespaces, objectMeta) {
+		glog.Infof("Skipping validation for %s/%s due to policy check", resourceNamespace, resourceName)
+		return &v1beta1.AdmissionResponse{
+			Allowed: true,
+		}
+	}
+	if objectMeta == nil {
+			return &v1beta1.AdmissionResponse{
+					Result: &metav1.Status{
+							Message: "[Skip] not pod",
+					},
+			}
+	}
+	annotations := map[string]string{admissionWebhookAnnotationStatusKey: "mutated"}
+	patchBytes, err := createPatch(&pod,availableAnnotations, annotations, availableLabels, addLabels)
+	if err != nil {
+		return &v1beta1.AdmissionResponse{
+			Result: &metav1.Status{
+				Message: err.Error(),
+			},
+		}
+	}
+
+	glog.Infof("AdmissionResponse: patch=%v\n", string(patchBytes))
+	return &v1beta1.AdmissionResponse{
+		Allowed: true,
+		Patch:   patchBytes,
+		PatchType: func() *v1beta1.PatchType {
+			pt := v1beta1.PatchTypeJSONPatch
+			return &pt
+		}(),
+	}
+}
+
+// Serve method for webhook server
+func (whsvr *WebhookServer) serve(w http.ResponseWriter, r *http.Request) {
+	var body []byte
+	if r.Body != nil {
+		if data, err := ioutil.ReadAll(r.Body); err == nil {
+			body = data
+		}
+	}
+	if len(body) == 0 {
+		glog.Error("empty body")
+		http.Error(w, "empty body", http.StatusBadRequest)
+		return
+	}
+
+	// verify the content type is accurate
+	contentType := r.Header.Get("Content-Type")
+	if contentType != "application/json" {
+		glog.Errorf("Content-Type=%s, expect application/json", contentType)
+		http.Error(w, "invalid Content-Type, expect `application/json`", http.StatusUnsupportedMediaType)
+		return
+	}
+
+	var admissionResponse *v1beta1.AdmissionResponse
+	ar := v1beta1.AdmissionReview{}
+	if _, _, err := deserializer.Decode(body, nil, &ar); err != nil {
+		glog.Errorf("Can't decode body: %v", err)
+		admissionResponse = &v1beta1.AdmissionResponse{
+			Result: &metav1.Status{
+				Message: err.Error(),
+			},
+		}
+	} else {
+		fmt.Println(r.URL.Path)
+		if r.URL.Path == "/mutate" {
+			admissionResponse = whsvr.mutate(&ar)
+		} else if r.URL.Path == "/validate" {
+			admissionResponse = whsvr.validate(&ar)
+		}
+	}
+
+	admissionReview := v1beta1.AdmissionReview{}
+	if admissionResponse != nil {
+		admissionReview.Response = admissionResponse
+		if ar.Request != nil {
+			admissionReview.Response.UID = ar.Request.UID
+		}
+	}
+
+	resp, err := json.Marshal(admissionReview)
+	if err != nil {
+		glog.Errorf("Can't encode response: %v", err)
+		http.Error(w, fmt.Sprintf("could not encode response: %v", err), http.StatusInternalServerError)
+	}
+	glog.Infof("Ready to write reponse ...")
+	if _, err := w.Write(resp); err != nil {
+		glog.Errorf("Can't write response: %v", err)
+		http.Error(w, fmt.Sprintf("could not write response: %v", err), http.StatusInternalServerError)
+	}
+}
